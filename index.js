a7f1e01df03c961c25ce3efdce2dbb5123a1f6e9
prashantgeek <pt15052000@gmail.com>
2025-11-16 07:30:51 +0530

    added listAll

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { S3Client, GetObjectCommand} from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand, PutObjectCommand, ListObjectsV2Command} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import dotenv from 'dotenv';
 
@@ -17,8 +17,33 @@ async function getObjectURL(objectKey) {
         Bucket: process.env.AWS_BUCKET_NAME,
         Key: objectKey
     });
-    const url = await getSignedUrl(s3Client, command, { expiresIn: 15 });
+    const url = await getSignedUrl(s3Client, command);
     return url;
 }
 
-console.log("Signed URL: ", await getObjectURL('screenshot.png'))
+async function putObjectURL(filename, contentType) {
+    const command = new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `uploads/user_uploads/${filename}`,
+        ContentType: contentType
+    });
+    const url = await getSignedUrl(s3Client, command);
+    return url;
+}
+
+async function listObjects() {
+    const command = new ListObjectsV2Command({
+        Bucket: process.env.AWS_BUCKET_NAME,
+    });
+    const response = await s3Client.send(command);
+    return response.Contents || [];
+}
+
+async function init() {
+    // console.log("Signed URL: ", await getObjectURL('uploads/user_uploads/image-1763256066823'));
+    //console.log("URL for uploading: ", await putObjectURL(`image-${Date.now()}`, 'image/png'));
+    const objects =  await listObjects();
+    console.log("Objects in bucket: ", objects);
+}
+
+init();
